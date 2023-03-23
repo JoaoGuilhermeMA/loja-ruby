5fdb56d42de0db1d2fc74f032408079be05b6e10
Joao Guilherme <joomoreira36@gmail.com>
2023-03-23 20:49:40 -0300

    estilizando as opções de departamento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_224827) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_231155) do
   create_table "departamentos", force: :cascade do |t|
     t.string "nome"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_224827) do
     t.integer "quantidade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "departamento_id"
   end
 
 end

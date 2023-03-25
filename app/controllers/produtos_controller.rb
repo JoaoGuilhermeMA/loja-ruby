73413ce7fff1d09fa29fac110ab0ca11aaca9eda
Joao Guilherme <joomoreira36@gmail.com>
2023-03-25 18:30:33 -0300

    refatorando o codigo

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,4 +1,7 @@
 class ProdutosController < ApplicationController
+
+  before_action :set_produto, only: [:edit, :update, :destroy]
+
   def index
     @produtos = Produto.order(nome: :asc).limit 6
     @produto_com_desconto = Produto.order(:preco).limit 1
@@ -10,44 +13,46 @@ class ProdutosController < ApplicationController
   end
 
   def edit
-    id = params[:id]
-    @produto = Produto.find(id)
     @departamentos = Departamento.all
     render :new
   end
 
   def update
-    id = params[:id]
-    @produto = Produto.find(id)
-    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-    if @produto.update valores
+    if @produto.update produto_params
       flash[:notice] = "Produto atualizado com sucesso"
       redirect_to root_url
     else
-      d@epartamentos = Departamento.all
+      @departamentos = Departamento.all
       render :new
     end
   end
 
   def create
-    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-    @produto = Produto.new valores
+    @produto = Produto.new produto_params
     if @produto.save
       flash[:notice] = "Produto salvo com sucesso!"
       redirect_to root_url
     else
+      @departamentos = Departamento.all
       render :new
     end
   end
 
   def destroy
-    id = params[:id]
-    Produto.destroy id
+    @produto.destroy
     redirect_to root_url
   end
 
   def busca
     @nome = params[:nome]
     @produtos = Produto.where "nome like ?", "%#{@nome}%"
+  end
+
+  def produto_params
+    params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+  end
+
+  def set_produto
+    @produto = Produto.find(params[:id])
   end
 end
